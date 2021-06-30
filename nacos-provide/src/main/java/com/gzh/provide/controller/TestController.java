23d1d59688be0e391fb8dfcd98af2684d6ecd5e2
admin <admin>
2021-06-30 10:40:27 +0800

    推送提交

---
+++ nacos-provide/src/main/java/com/gzh/provide/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.gzh.provide.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/helloNacos")
+    public String helloNacos() {
+        return "你好，nacos！";
+    }
+}
